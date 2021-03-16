1edc5adcf7c4d5c84866f975853f7ad046c081d7
jfout <jfout@lakotasoftware.com>
2021-03-16 14:11:20 -0400

    First Commit

--- src/test/java/ip/swagger/petstore/PetStoreTest.java
+++ src/test/java/ip/swagger/petstore/PetStoreTest.java
@@ -1,5 +1,11 @@
 package ip.swagger.petstore;
 
 public class PetStoreTest {
+    
+    @Test
+    public void testPrintMessage() {	
+       System.out.println("Inside testPrintMessage()");    
+       assertEquals(message, messageUtil.printMessage());     
+    }
 
 }
